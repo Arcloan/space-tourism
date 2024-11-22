41898474dbe08a027c27d2f33609378818f9a159
Arcloan <95delchris@gmail.com>
2024-11-22 19:07:31 +0100

    first draft, done the homepage

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        white: 'hsl(0, 0%, 100%)',
+        lightBlue: 'hsl(231, 77%, 90%)',
+        darkBlue: 'hsl(230, 35%, 7%)',
+      },
+      fontFamily: {
+        "Barlow": ["Barlow", "sans-serif"],
+        "Bellefair": ["Bellefair", "sans-serif"],
+      }
+    },
+  },
+  plugins: [],
+}
+
